218d2b139ffd90d21df512592bb92b82dd3de6b9
전찬호 <JChanho99@gmail.com>
2020-06-02 23:02:33 +0900

    본격적으로 들어갑니데이

--- CPP_SOURCE/main.cpp
+++ CPP_SOURCE/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <string>
 using namespace std;
 
 
 int main()
 {
-    cout << "Git Auto-Writing Program";
-
+    string inputStr;
     
+    cout << "Git Auto-Writing Program\n" << endl;
+    cout << "문구를 입력하세요\n" << endl;
+    cin >> inputStr;
+    
+    ofstream ofs("TESTFILE.txt");
+    ofs << inputStr;
+    ofs.close();
+    
+    ofs.is_open();
+    
+    return 0;
 }
